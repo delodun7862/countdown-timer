6b9bb115328ee45282b80639cdb7650dceaf06e6
delodun7862 <adelodunkanyinsola@gmail.com>
2024-02-29 09:17:30 +0000

    first commit

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
 
-var countDownDate = new Date("Mar 7, 2024").getTime();
+  const countDownDate = new Date();
+  countDownDate.setDate(countDownDate.getDate() + 10);
 
-var x = setInterval(function(){
-  
-    var now = new Date().getTime();
-    
-    var distance = countDownDate - now;
-    
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    function updateCountdown() {
+      const currentDate = new Date();
+      const distance = countDownDate  - currentDate;
 
+      if (distance <= 0) {
+        clearInterval(timerInterval);
+        document.getElementById('countdown').innerHTML = 'Countdown Over!';
+      } else {
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById("days").innerHTML = days;
-    document.getElementById("hours").innerHTML = hours;
-    document.getElementById("minute").innerHTML = minutes;
-    document.getElementById("seconds").innerHTML = seconds;
-
-    if(distance < 0){
-        clearInterval(x);
-        document.getElementById("days").innerHTML = "00";
-        document.getElementById("hours").innerHTML = "00";
-        document.getElementById("minute").innerHTML = "00";
-        document.getElementById("seconds").innerHTML = "00";
+        document.getElementById('days').innerHTML = days;
+        document.getElementById('hours').innerHTML = hours;
+        document.getElementById('minutes').innerHTML = minutes;
+        document.getElementById('seconds').innerHTML = seconds;
+      }
     }
-    
-    
-},1000);
+
+    const timerInterval = setInterval(updateCountdown, 1000);
+
+    const endDate = countDownDate.toLocaleString('en-US', { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', });
+      document.getElementById('giveaway-info').innerHTML = `Giveaway Ends On ${endDate} at 11:30am`;
